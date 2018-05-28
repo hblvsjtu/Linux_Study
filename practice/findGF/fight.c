6e38f77b54db910addf293e87674151f90c37d88
hblvsjtu <hblvsjtu@163.com>
2018-05-28 16:05:54 +0800

    2018-05-28
    1,更新了第12章软件安装

---
+++ practice/findGF/fight.c
@@ -0,0 +1,26 @@
+/* ***************************************************************
+ *
+ * * Filename: Fight.c
+ *
+ * * Description:linking and compiling test
+ *
+ * * Version: 1.0
+ *
+ * * Created: 2018/05/28
+ *
+ * * Revision: none
+ *
+ * * Compiler: gcc
+ *
+ * * Author: Lv Hongbin
+ *
+ * * Company: Shanghai JiaoTong Univerity
+ *
+ * * **************************************************************/
+
+#include<stdio.h>
+void fight(void) {
+	printf("I'm learning C programming language! \n");
+	printf("I'm earning money! \n");
+	printf("I'm keeping fit! \n");
+}
---
+++ practice/findGF/findGF.c
@@ -0,0 +1,25 @@
+/* ***************************************************************
+ *
+ * * Filename: FindGF.c
+ *
+ * * Description:linking and compiling test
+ *
+ * * Version: 1.0
+ *
+ * * Created: 2018/05/28
+ *
+ * * Revision: none
+ *
+ * * Compiler: gcc
+ *
+ * * Author: Lv Hongbin
+ *
+ * * Company: Shanghai JiaoTong Univerity
+ *
+ * * **************************************************************/
+
+#include<stdio.h>
+int main(void) {
+	printf("Let's look for a girl\n");
+	fight();
+}
